ac45dac6c0760386b94d878f2dd72d0af3bb9d63
Svetik <sotnikovas337@gmail.com>
2025-05-11 18:38:54 +0300

    add config

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// config/config.php
+
+// Можно загрузить из .env, но здесь просто определим напрямую
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'labbd');
+define('DB_USER', 'root');
+define('DB_PASS', 'root'); 
+?>
